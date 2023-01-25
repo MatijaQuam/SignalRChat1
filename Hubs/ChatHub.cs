689f0ab8381ec44c7eafbecab4b45c77ccf5d774
QUAMatik <64063167+QUAMatik@users.noreply.github.com>
2023-01-25 15:59:24 +0100

    First commit.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+       public Task SendMessageToCaller(string user, string message)
+        {
+           return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToGroup(string user, string message, string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message+" ->"+groupName);
+        }
+
+        
+       /* public async Task JoinGroup(string user,string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessageJoin",user +" "+ Context.ConnectionId, groupName);
+        }*/
+      
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,75 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+document.getElementById("sendPrivateButton").disabled = true;
+document.getElementById("sendGroupMessageButton").disabled = true;
+// document.getElementById("joinButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user}: ${message}`;
+});
+
+connection.on("ReceiveMessageJoin", function (user,groupName) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} joins ${groupName}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    document.getElementById("sendPrivateButton").disabled = false;
+    document.getElementById("sendGroupMessageButton").disabled = false;
+    // document.getElementById("joinButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendPrivateButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessageToCaller", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendGroupMessageButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var groupName = document.getElementById("groupNameInput").value;
+    connection.invoke("SendMessageToGroup", user, message, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
+/*document.getElementById("joinButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var groupName = document.getElementById("groupNameInput").value;
+    connection.invoke("JoinGroup",user, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});*/
