7a009583e3e43deb6e1ed4c4421a9a49339142ce
MatijaQuam <matija.orsulic@quam.hr>
2023-01-26 16:00:26 +0100

    GUI update, log files created.

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -2,33 +2,101 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
 
-       public Task SendMessageToCaller(string user, string message)
+        private static void LogWrite(string logMessage, StreamWriter w)
         {
-           return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+            w.WriteLine("{0}", logMessage);
+            w.WriteLine("----------------------------------------");
+        }
+        public void writeLog(string strValue,string name)
+        {
+            try
+            {
+                //Logfile
+                string path = @"C:\Users\lukaq\source\repos\logdata\";
+                path = path + name + ".log";
+                StreamWriter sw;
+                if (!File.Exists(path))
+                { sw = File.CreateText(path); }
+                else
+                { sw = File.AppendText(path); }
+
+                LogWrite(strValue, sw);
+
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         public async Task SendMessageToGroup(string user, string message, string groupName)
         {
+            string logDateTime = DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss");
+            string time = DateTime.UtcNow.ToString("HH:mm");
+
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message+" ->"+groupName);
+            await Clients.Caller.SendAsync("ReceiveMessage", user + " |" + time + "|", message);
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user + " |"+time+"| ", message);
+
+            string name = user + Context.ConnectionId;
+            string strValue = "User name: " +user + ", Group name: " + groupName + ", Date and Time: " + logDateTime + " Message: " + message + ", Action: " + "message send.";
+            writeLog(strValue, name);
+        }
+        public async Task JoinGroup(string user, string groupName)
+        {
+            string logDateTime = DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss");
+            string time = DateTime.UtcNow.ToString("HH:mm");
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.SendAsync("ReceiveMessage", user, " you have joined the chat at " + time + ".");
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user, " has join your chat at" + time + ".");
+
+            string name = user + Context.ConnectionId;
+            string strValue = "User name: " + user + ", Group name: " + groupName + ", Date and Time: " + logDateTime + ", Action: " + "Join group.";
+            writeLog(strValue, name);
+        }
+        public async Task LeaveGroup(string user, string groupName)
+        {
+            string logDateTime = DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss");
+            string time = DateTime.UtcNow.ToString("HH:mm");
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.SendAsync("ReceiveMessage", user, " you have left the chat " + time + ".");
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user, " has left your chat at "+ time +".");
+
+            string name = user + Context.ConnectionId;
+            string strValue = "User name: " + user + ", Group name: " + groupName + ", Date and Time: " + logDateTime + ", Action: " + "Leave group.";
+            writeLog(strValue, name);
         }
 
-        
-       /* public async Task JoinGroup(string user,string groupName)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("ReceiveMessageJoin",user +" "+ Context.ConnectionId, groupName);
-        }*/
-      
+
+
+        //OLD CODE
+
+        /*public async Task SendMessage(string user, string message)
+          {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+          } */
+
+        /*public Task SendMessageToCaller(string user, string message)
+         {
+           return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+          }*/
+
+        /*public async Task JoinGroup(string user,string groupName)
+         {  
+             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+             await Clients.Group(groupName).SendAsync("ReceiveMessageJoin",user +" "+ Context.ConnectionId, groupName);
+         }*/
+
     }
 }
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -4,32 +4,44 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
-document.getElementById("sendPrivateButton").disabled = true;
-document.getElementById("sendGroupMessageButton").disabled = true
+document.getElementById("joinButton").disabled = true;
+document.getElementById("leaveButton").disabled = true;
+// document.getElementById("sendGroupMessageButton").disabled = true
 
 connection.on("ReceiveMessage", function (user, message) {
+    var p = document.createElement("p");
+    document.getElementById("messagesList").appendChild(p);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    p.textContent = `${user}: ${message}`;
+});
+/*connection.on("ReceiveMessage", function (user, message) {
+    var div = document.createElement("div");
+    div.style = "border - radius: 15px; background - color: rgba(57, 192, 237, .2)"
+    div.className = "p - 3 me - 3 border"
+    document.getElementById("father").appendChild(div);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    div.textContent = `${user}: ${message}`;
+});*/
+/*
+connection.on("MyMessage", function (user, message) {
     var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
+    document.getElementById("messagesListMe").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
     li.textContent = `${user}: ${message}`;
 });
-
-connection.on("ReceiveMessageJoin", function (user,groupName) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} joins ${groupName}`;
-});
+*/
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
-    document.getElementById("sendPrivateButton").disabled = false;
-    document.getElementById("sendGroupMessageButton").disabled = false;
-    // document.getElementById("joinButton").disabled = false;
+    // document.getElementById("sendPrivateButton").disabled = false;
+    // document.getElementById("sendGroupMessageButton").disabled = false;
+    document.getElementById("joinButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
@@ -38,12 +50,55 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var groupName = document.getElementById("groupNameInput").value;
+    document.getElementById("messageInput").value="";
+    connection.invoke("SendMessageToGroup", user, message, groupName).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
 
+document.getElementById("joinButton").addEventListener("click", function (event) {
+
+    document.getElementById("joinButton").disabled = true;
+    document.getElementById("joinButton").hidden = true;
+    document.getElementById("leaveButton").disabled = false;
+    document.getElementById("leaveButton").hidden = false;
+    document.getElementById("sendButton").hidden = false;
+    document.getElementById("userInput").disabled = true;
+    document.getElementById("groupNameInput").disabled = true;
+    document.getElementById("messageInput").disabled = false;
+
+    var user = document.getElementById("userInput").value;
+    var groupName = document.getElementById("groupNameInput").value;
+
+    connection.invoke("JoinGroup", user, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("leaveButton").addEventListener("click", function (event) {
+
+    document.getElementById("leaveButton").disabled = true;
+    document.getElementById("leaveButton").hidden = true;
+    document.getElementById("joinButton").disabled = false;
+    document.getElementById("joinButton").hidden = false;
+    document.getElementById("sendButton").hidden = true;
+    document.getElementById("userInput").disabled = false
+    document.getElementById("groupNameInput").disabled = false;
+    document.getElementById("messageInput").disabled = true;
+    document.getElementById("messagesList").replaceChildren();
+
+    var user = document.getElementById("userInput").value;
+    var groupName = document.getElementById("groupNameInput").value;
+    connection.invoke("LeaveGroup", user, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+/*
 document.getElementById("sendPrivateButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
@@ -52,7 +107,9 @@ document.getElementById("sendPrivateButton").addEventListener("click", function 
     });
     event.preventDefault();
 });
+*/
 
+/*
 document.getElementById("sendGroupMessageButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
@@ -62,6 +119,7 @@ document.getElementById("sendGroupMessageButton").addEventListener("click", func
     });
     event.preventDefault();
 });
+*/
 
 
 /*document.getElementById("joinButton").addEventListener("click", function (event) {
